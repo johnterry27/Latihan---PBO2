970a7b403cf80b2982497aa2b2c46a095b59df46
johnterry27 <161942169+johnterry27@users.noreply.github.com>
2025-03-27 18:22:11 +0700

    Add files via upload

---
+++ Pbo8.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Pbo8 {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+
+        v.add("A");
+        v.add("B");
+        v.add("D");
+        v.add("E");
+        v.add("F");
+
+        System.out.println("Vector contains : " + v);
+
+        Enumeration e = v.elements();
+
+        ArrayList aList = Collections.list(e);
+        System.out.println("Arraylist contains : " + aList);
+    }
+}
