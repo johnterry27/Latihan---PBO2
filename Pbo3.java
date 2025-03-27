102790c54f4df6e594623127bfa6455c216d3602
johnterry27 <161942169+johnterry27@users.noreply.github.com>
2025-03-27 18:07:10 +0700

    Add files via upload

---
+++ Pbo3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Pbo3 {
+    public static void main(String[] args) {
+        String[] words = {"saya", "ingin", "lulus", "saya", "akan", "berusaha"};
+        Set<String> uniques = new HashSet<String>();
+        Set<String> dups = new HashSet<String>();
+
+        for (String a : words)
+            if (!uniques.add(a))
+                dups.add(a);
+
+        uniques.removeAll(dups);
+        System.out.println("Unique words: " + uniques);
+        System.out.println("Duplicate words: " + dups);
+    }
+}
